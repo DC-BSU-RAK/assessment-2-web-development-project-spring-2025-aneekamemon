2e6a00ee8e3bb563e913a882121c918fac9423f8
aneekamemon <aneekamemon13@gmail.com>
2025-05-18 19:57:08 +0400

    shop.js

--- shop.js
+++ shop.js
@@ -3,54 +3,71 @@
 
 // This is the section for the night and light mode stickers 
 
-
+    // this is the part of the js code that brings forth the button used to 
+    // toggle between the 2 themes of dark and light mode
 const themeFROST = document.getElementById('theme');
+    // After it has gotten the element by its ID theme it will get the image inside of it
+    // keeping in mind there are 2 seperate images of both the light and dark modes
 const themestickersFROST = document.getElementById('themesticker');
 
-function Themes() {
-    const savedtheme = localStorage.getItem('theme');
-    const systemPrefersDark = window.matchMedia('(prefers-color-scheme:dark').matches;
-    const currentTheme = savedtheme || (systemPrefersDark ? 'dark' : 'light' );
+function themeforthewebpage() {
 
-    if (currentTheme === 'dark') {
+    // function themes is the function being declared as the page starts to load
+    const changedatheme = document.getElementById('theme');
+
+    // This part uses matchmedia api to conclude if the user is currently using dark
+    // or light mode
+    const previouspick = window.matchMedia('(userpicks:dark)').matches;
+
+    const currentTheme = changedatheme || (previouspick ? 'dark' : 'light' );
+
+    if (currentTheme ==='dark') {
 
         document.documentElement.setAttribute('data-theme', 'dark');
-        themestickersFROST.src = 'stickers/lightmode.png';
+        // This part makes sure if the chosen theme is dark
+        // if it is then it will show the light mode sticker so the user can click that
+        // if they want to change the theme
+        themestickersFROST.image ='stickers/lightmode.png';
 
-    }   else {
+    }   else {  
         document.documentElement.setAttribute('data-theme', 'light');
-        themestickersFROST.src = 'stickers/darkmode.png';
+        // if not then the theme stays on light mode and the image shown will be the dark mode on
+        // encouraging the user to change between them 
+        themestickersFROST.image ='stickers/darkmode.png';
     }
 }
 
 themeFROST.addEventListener('click', () => {
-
+    // This detects if the user has clicked the frost toggle theme button and changes the background and rest of the colors accordingly
     const currentmode = document.documentElement.getAttribute('data-theme');
 
-    if (currentmode === 'dark') {
+    if (currentmode ==='dark') {
 
         document.documentElement.setAttribute('data-theme', 'light');
+
+        // if there is a click on the darkmode sticker then the theme changes to light
         localStorage.setItem('theme', 'light');
-        themestickersFROST.src = 'stickers/darkmode.png';
+        themestickersFROST.image = 'stickers/darkmode.png';
 
     }   else {
         document.documentElement.setAttribute('data-theme', 'dark');
+        // If not then it stays the same and displays the lightmode sticker
         localStorage.setItem('theme', 'dark');
-        themestickersFROST.src = 'stickers/lightmode.png';
+        themestickersFROST.image = 'stickers/lightmode.png';
 
     }
 });
 
-Themes();
+themeforthewebpage();
 
-window.matchMedia('(prefers-color-scheme: dark)').addEventListener('change', (yurp) => {
+window.matchMedia('(userpicks: dark)').addEventListener('change', (yurp) => {
     if (!localStorage.getItem('theme')) {
         if (yurp.matches) {
             document.documentElement.setAttribute('data-theme', 'dark');
-            themestickersFROST.src = 'lightmode.png';
+            themestickersFROST.image = 'lightmode.png';
         }   else {
             document.documentElement.removeAttribute('data-theme');
-            themestickersFROST.src = 'lightmode.png';
+            themestickersFROST.image = 'lightmode.png';
         }
     }
 });
@@ -62,14 +79,15 @@ const verifycontainer = document.querySelectorAll('.verifycontainer');
 const descripcontainer = document.querySelectorAll('.descripcontainer');
 const conditionscontainer = document.querySelectorAll('.conditionscontainer');
 
-
+// This is where the slide in effect takes places where the item is translated from either X or Y 
+// and depending on the input placed in the css files.
+// The effect works when .show is added alongside the name of a given element or tag to bring back the element or item to its original place and
+// increase the opacity so it can be seen again
 
 const ob = new IntersectionObserver(entries => {
     entries.forEach(entry => {
         if (entry.isIntersecting) {
             entry.target.classList.add('show');
-        }   else {
-            entry.target.classList.remove('show');
         }
     }); 
 });
@@ -79,7 +97,7 @@ descripcontainer.forEach(el => ob.observe(el));
 conditionscontainer.forEach(el => ob.observe(el));
 
 
-
+// This section showcases the hidden information that can only been seen when clicked upon
 const watches = [
     {
         name: "Chronograph Platinum Green",
@@ -162,12 +180,23 @@ const watches = [
     },
 ];
 
-let index = 0;
+// these are all the information related to specific watches
+
+// ring keeps track of the amount of watches being displayed
+
+let ring = 0;
+
+// loaditems will only load 5 watches at a time, even when clicked the load more button
 const loaditems = 5;
+
+// all watches their cards and information are placed in the grid container in html 
 const grid = document.getElementById("grid");
 
-function createBox(watch) {
+
+
+function infoonallwatches(watch) {
     const card = document.createElement("div");
+    // This creates a new div to add all information down below
     card.className = "watchbox grid";
     card.setAttribute("data-name", watch.name);
     card.setAttribute("data-price", watch.price);
@@ -187,18 +216,20 @@ function createBox(watch) {
 }
 
 function loadmore() {
-    const moreitems = watches.slice(index, index + loaditems);
+    const moreitems = watches.slice(ring, ring + loaditems);
+    // This function is used when the load more button is pressed and loads 5 more watches and
+    // their information accordingly
     moreitems.forEach(watch => {
-        grid.appendChild(createBox(watch));
+        grid.appendChild(infoonallwatches(watch));
 
     });
-    index+= loaditems;
+    ring+= loaditems;
 }
 
 document.getElementById("loadmorebutton").addEventListener("click", loadmore);
-
+// This waits to observe if the loadmorebutton gets pressed or not 
 window.onload = loadmore;
-
+// This loads the first set of watches onto the page
 const productwatch = document.getElementById("productwatch");
 
 const watchImage = document.getElementById("watchImage");
@@ -218,7 +249,7 @@ const watchdescription = document.getElementById("watchdescription");
 const closebutton = document.getElementById("closebutton");
 
 const heartbutton = document.getElementById("heartbutton");
-
+// All these are used for the elements inside the hidden display that showcases all the watches
 
 
 
@@ -231,9 +262,13 @@ function showwatch(data) {
     company.textContent = data.company;
     size.textContent = data.size;
     watchdescription.textContent = data.description;
+    // This showcases all information and hides the hidden element so the display can
+    // be seen
     productwatch.classList.remove("hidden")
 }
 
+
+// adds back the hidewatch function so that the user can view more watches once they have clicked x.
 function hidewatch() {
     productwatch.classList.add("hidden");
 }
@@ -242,20 +277,24 @@ closebutton.addEventListener("click", hidewatch);
 heartbutton.addEventListener("click",() => {
     heartbutton.classList.toggle("like");
 });
-
+// if the heart button is clicked the .like class is toggled which
+// then changes the color of the heart to maroon
 grid.addEventListener("click", (e) => {
     const card = e.target.closest(".watchbox");
     if (!card) return;
     const data = {
         name:card.dataset.name,
+
         price:card.dataset.price,
         type:card.dataset.type,
+
         company:card.dataset.company,
         size:card.dataset.size,
         image:card.dataset.image,
         description:card.dataset.description,
     };
-
+    // when a specific watch is clicked then all this data above
+    // is showcased 
     showwatch(data);
 });
 
